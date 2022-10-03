71bbb01b290dff0ef3311e61a42d445aaa6942a7
Joshua Arulsamy <joshua.gf.arul@gmail.com>
2022-10-03 16:42:39 -0600

    Add some much needed comments

--- cpp/src/board.hpp
+++ cpp/src/board.hpp
@@ -14,6 +14,7 @@ constexpr char X = 'X';
 constexpr char O = 'O';
 constexpr char EMPTY = '0';
 
+// Minimax
 constexpr char MAX_PLAYER = O;
 constexpr char MIN_PLAYER = X;
 
@@ -21,12 +22,17 @@ constexpr char MIN_PLAYER = X;
 constexpr size_t START_X = 2;
 constexpr size_t START_Y = 2;
 
+// Board dimensions
 constexpr size_t CELL_WIDTH = 4;
 constexpr size_t CELL_HEIGHT = 2;
-
 constexpr int BOARD_DIM = 3;
 }  // namespace constants
 
+// Helper data structures ------------------------------------------------------
+
+/**
+ * @brief A simple representation of a point in 2D space.
+ */
 template <typename T>
 struct Pt
 {
@@ -38,6 +44,10 @@ struct Pt
 template <typename T>
 std::ostream& operator<<(std::ostream& os, const Pt<T>& pt);
 
+/**
+ * @brief A simple representation of a point in 2D space, type specialized for
+ *        int for the sake of more sane default values.
+ */
 template <>
 struct Pt<int>
 {
@@ -47,6 +57,10 @@ struct Pt<int>
   Pt(int X, int Y) : x(X), y(Y){};
 };
 
+/**
+ * @brief Represents one 'square' within the TicTacToe board. Stores some extra
+ *        info to make win checking a bit easier.
+ */
 struct Cell
 {
   Pt<int> pos;
@@ -59,6 +73,9 @@ struct Cell
 
 std::ostream& operator<<(std::ostream& os, const Cell& cell);
 
+/**
+ * @brief Represents one move, placing a 'X' or 'O' within a square.
+ */
 struct Move
 {
   Cell* cell;
@@ -75,49 +92,152 @@ std::ostream& operator<<(
     std::ostream& os,
     const std::array<Cell, constants::BOARD_DIM * constants::BOARD_DIM> board);
 
+// Core ------------------------------------------------------------------------
+/**
+ * @brief Bulk of the game logic.
+ */
 class Board
 {
  private:
+  // Storage of the game board positions.
   // +---+---+---+
-  // | 0 | 1 | 2 |
+  // | 0 | 3 | 6 |
   // +---+---+---+
-  // | 3 | 4 | 5 |
+  // | 1 | 4 | 7 |
   // +---+---+---+
-  // | 6 | 7 | 8 |
+  // | 2 | 5 | 8 |
   // +---+---+---+
   std::array<Cell, constants::BOARD_DIM * constants::BOARD_DIM> cells;
+
+  // Minimum and maximum coordinates of cells within terminal.
+  // Used to keep the cursor within the board at all times.
   const Pt<int> min;
   const Pt<int> max;
 
-  // Minimax
+  /**
+   * @brief Compute a move given the current board state and player using
+   * the minimax algorithm. Recursive implementation.
+   *
+   * @param depth: Depth of the recursion.
+   * @param player: Current player to apply algorithm within.
+   * @return: Max or min scored move (depends on player).
+   */
   Move minimax(const int depth, const char player);
 
  public:
+  /**
+   * @brief Initialize the ncurses environment and display the gameboard.
+   */
   Board();
   ~Board();
 
-  // UI
+  /**
+   * @brief Draw the gameboard and any other visual attributes.
+   */
   void draw() const;
 
   // Movement
+  /**
+   * @brief Move the cursor.
+   *
+   * @param pt: Absolute point on the terminal to move cursor to.
+   * @return: ERR on error, OK otherwise.
+   */
   int move_cursor(const Pt<int>& pt) const;
+
+  /**
+   * @brief Move the cursor.
+   *
+   * @param index: Cell index to move cursor to.
+   * @return: ERR on error, OK otherwise.
+   */
   int move_cursor(const int& index) const;
+
+  /**
+   * @brief Center the cursor within the gameboard.
+   *
+   * TODO: Currently broken for gameboard sizes larger than 3x3.
+   *
+   * @return: ERR on error, OK otherwise.
+   */
   int center_cursor() const;
 
+  /**
+   * @brief Handle cursor movements within the gameboard.
+   *
+   * @param pt: Output point the cursor was on when the user pressed SPC.
+   * @return: True if the user pressed a valid entry key, false if the user
+   *          wants to terminate the program.
+   */
   bool handle_cursor(Pt<int>* pt) const;
 
-  // Game logic
+  /**
+   * @brief Check if a player has won the game given the last move.
+   *        Since we have the last move, the search space within the board is
+   *        generaly smaller. This typically performs better than any other
+   *        overload, so use this whenver possible.
+   *
+   * @param player: Player to check win condition for.
+   * @param last_move: Index of cell of last played position.
+   * @return: True if player won, false otherwise.
+   */
   bool check_win(const char& player, const int& last_move) const;
+
+  /**
+   * @brief Check if a player has won the game given the last move.
+   *        Searches the entire board for win conditions. Avoid using this for
+   *        the sake of performance whenever possible.
+   *
+   * @param player: Player to check win condition for.
+   * @return: True if player won, false otherwise.
+   */
   bool check_win(const char& player) const;
+
+  /**
+   * @brief Determine if any playable cells exist.
+   *
+   * @return: True if no playable cells exist, false otherwise.
+   */
   bool is_game_over() const;
+
+  /**
+   * @brief Display a message depending on which player won, or if a stalemate
+   *        occured.
+   *
+   * @param: Player that won. If player is 'EMPTY' display a stalemate message.
+   */
   void win_handler(const char& player) const;
+
+  /**
+   * @brief Find all playable cells given the current board state.
+   *
+   * @return: Vector of all playable cells.
+   */
   std::vector<Cell*> get_available_cells() const;
 
+  /**
+   * @brief Handy overload simplifying the calling of the private 'minimax'.
+   *
+   * @return: Move of 'MAX' player given the current board state.
+   */
   Move minimax();
 
   // Game modes
+  // TODO: Some simplification here is likely worthwhile.
+
+  /**
+   * @brief Play a game human vs. human.
+   */
   void v_human();
+
+  /**
+   * @brief Play a game human vs. RNG.
+   */
   void v_random();
+
+  /**
+   * @brief Play a game human vs. minimax algorithm.
+   */
   void v_minimax();
 };
 
